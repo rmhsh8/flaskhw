b749575321d8400ff84bf0731f0ab67c4601e4e9
rmhsh8 <99620775+rmhsh8@users.noreply.github.com>
2022-03-28 18:26:06 -0700

    Add files via upload

---
+++ flask hw/home.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import jinja2
+
+app = Flask(__name__)
+app.config['debug'] =True
+
+@app.route('/')
+def main():
+    name = 'Lisa'
+    city_names = ["Paris", "London", "Rome", "Tahiti"]
+    return '''
+
+<html>
+    </head>
+    <body>
+        <h1>Welcome ''' + name + ''' </h1>
+        <p>
+            <a href = "https://www.google.com/" > not google</a>
+            <ul>
+                <li> ''' + city_names[0] + ''' </li>
+                <li> ''' + city_names[1] + ''' </li>
+                <li> ''' + city_names[2] + ''' </li>
+                <li> ''' + city_names[3] + ''' </li>
+                 </ul>
+        </p>
+    </body>
+</html> 
+'''
+app.run()
